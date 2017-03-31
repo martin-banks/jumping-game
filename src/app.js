777275630c9ff649bf0c9a45c2bdf3f9d0b06d88
Martin Banks <anotherbanksy@gmail.com>
2017-03-31 08:08:58 +0400

    add start/stop button and random obstacles

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@
 
 
 const game = {
+	isActive: false,
 	data:{
 		score: 0,
 		level: 1
@@ -21,14 +22,31 @@ const scene3 = new SceneTemplate()
 
 const scenes = [
 	scene1.render('first'),
-	scene2.render('second'),
-	/*scene3.render ('complete')*/
+	scene2.render('second')
 ]
 
-document.querySelector('#app').innerHTML = scenes.join('')
+document.querySelector('#app').innerHTML = [
+	`<div id="gameContainer">${scenes.join('')}</div>`,
+	`<button id='startButton' type="button" class="btn btn-default">New Game</button>`
+].join('')
 
-scene1.playScene({startPos: 0, id:'first'})
-scene2.playScene({startPos: 100, id:'second'})
+document.getElementById('startButton').addEventListener('click', e =>{
+	scene1.toggleStatus()
+	scene2.toggleStatus()
+	if (game.isActive){
+		/* game is runnning: stop it */
+		game.isActive = false
+		e.target.innerText = 'New Game'
+	} else {
+		/* game is not running: start it */
+		game.isActive = true
+		scene1.playScene({startPos: 0, id:'first'})
+		scene2.playScene({startPos: 100, id:'second'})
+		e.target.innerText = 'End game'
+	}
+	
+})
+
 
 /*setTimeout(()=>{
 	document.querySelector('.scene').classList.add('scene-playing')	
--- src/sceneTemplate.js
+++ src/sceneTemplate.js
@@ -4,10 +4,24 @@ class SceneTemplate {
 		this.template = this.template.bind(this)
 		this.playScene = this.playScene.bind(this)
 		this.loadScene = this.loadScene.bind(this)
+		this.toggleStatus = this.toggleStatus.bind(this)
+
+		this.state = {
+			playing: false,
+			gameSpeed: 10 * 1000
+		}
 	}
 
 
-	
+	toggleStatus(){
+		let {playing} = this.state
+		if (playing === true){
+			this.state.playing = false
+		} else {
+			this.state.playing = true
+		}
+		console.log(this.state)
+	}
 
 	template (id){
 		return `
@@ -17,44 +31,68 @@ class SceneTemplate {
 
 	loadScene(){
 		/* render random obstacles */
-		return 'helo world'
+		return 'hello world'
 	}
 
 
 	playScene({id, startPos=100}={}){
 		/* animate through to complete */
+		let isPlaying = ()=> this.state.playing
 		let animStart = null
 		let elem = document.getElementById(id)
 		let start = startPos
-		console.log(elem)
-		let duration = 3000
+		let {gameSpeed} = this.state
+		let duration = gameSpeed
 		function step(timestamp) {
 			if (!animStart) animStart = timestamp
 			let progress = timestamp - animStart
 			let increment = ()=> {
-				let inc = progress/duration * 100
+				let inc = Math.min((progress/duration*100), 100)
+				//console.log('increment', inc)
 				if (start === 100){
 					return 2 * inc
 				} else if ( start === 0 ){
-					duration = 1500
+					duration = gameSpeed / 2
 					return inc
 				}
 				
 			}
-			console.log(increment())
+			//console.log(increment())
 			
 			/* do animation */
+
 			elem.style.transform = `translate3d(${start - (increment())}%, 0, 0)`
-			if(progress < duration){
+			if(progress < duration && isPlaying()){
 				window.requestAnimationFrame(step)
+
+			} else if (!isPlaying()){
+				console.log('stopped')
+				return
 			} else {
+				console.log('restart', isPlaying())
 				elem.style.transform = `translate3d(${start}%,0,0)`
 				animStart = null
 				start = 100
-				duration = 3000
+				duration = gameSpeed
+				const obstacleSizes = [
+				'small', 'medium', 'large'
+				]
+				const obstacles = ()=>{
+					let tmp = []
+					for(let i=0; i< Math.ceil(Math.random()*2)+1; i++){
+						tmp.push(`
+							<div 
+								class="obstacle ${obstacleSizes[Math.ceil(Math.random()*obstacleSizes.length)-1]}"
+								style="transform: translate3d(${ 500/100 * (Math.ceil( (Math.random()*100 )+1)) }px, 0, 0)"
+							></div>`
+						)
+					}
+					return tmp.join('')
+				}
+				elem.innerHTML = obstacles()
 				// comment out animation call to prevent infinite 
 				// recursion until start/stop is in place
-				//window.requestAnimationFrame(step)
+				window.requestAnimationFrame(step)
 			}
 		}	
 		window.requestAnimationFrame(step)
