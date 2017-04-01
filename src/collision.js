e5a09b8e63210dcf1afc5a791fc3ae938d3aa9f6
Martin Banks <anotherbanksy@gmail.com>
2017-04-01 18:15:37 +0400

    begin collision set up

---
+++ src/collision.js
@@ -0,0 +1,30 @@
+class Collision {
+	constructor(){
+		this.window = this.window.bind(this)
+		this.check = this.check.bind(this)
+		this.point = this.point.bind(this)
+	}
+
+	window(){
+		/* check if hte player is in a collision window */
+		/* this should also work for suare detection */
+		let horizontal = 
+		let vertical
+		if( !!hotizontal ){
+			return !! vertical ? true : false
+		} else {
+			return false
+		}
+		
+	}
+
+	point(){
+		return {
+			player: null,
+			obstacle: null
+		}
+	}
+	check(){
+
+	}
+}
